f2889620d35dfff573c7326f238ff752b7499357
imago <imago2000@imagomatomacbook-pro.local>
2016-05-19 09:51:03 +0800

    Initial Commit

---
+++ Learn01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learn01
+//
+//  Created by imago on 16/5/19.
+//  Copyright © 2016年 imago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
